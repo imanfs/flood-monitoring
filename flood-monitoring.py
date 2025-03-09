8f134d35aeb74515aff3a0371b077eff7026efa1
imanfs <imanfsanai@gmail.com>
2025-03-09 12:21:21 +0000

    speedups

--- flood-monitoring.py
+++ flood-monitoring.py
@@ -11,44 +11,68 @@ class MeasurementTool:
     ):
         self.root = "https://environment.data.gov.uk/flood-monitoring"
         self.page_url = self.root + "/id/stations/"
-        self.page = requests.get(self.page_url)
-        self.entries = self.page.json()["items"]
-        self.stations = self.create_stations_dict()
-        self.data = {}
-        self.measure_metadata = {}
-        latest_possible_reading_time = self.get_rounded_timestamp()
-        self.last_24_hrs = self.subtract_24_hours(latest_possible_reading_time)
+        self.session = requests.Session()  # Use a session for connection pooling
+        curr_timestamp_rounded = (
+            self.get_rounded_current_time()
+        )  # rounded down to last 15 m interval
+        self.last_24_hrs = self.subtract_24_hours(curr_timestamp_rounded)
+
+        self.stations = self._initialize_stations()
+
+    def _initialize_stations(self):
+        """Initialize stations dictionary with caching.
+        Maps station label to its reference code."""
+        page = self.session.get(self.page_url)
+        entries = page.json()["items"]
+
+        stations_data = {}
+        for entry in entries:
+            label = entry["label"]
+            if isinstance(label, list):
+                label = entry["label"][0]
+            stations_data[entry["stationReference"]] = label.title()
+        return stations_data
 
     def retrieve_metadata(self, url):
-        page = requests.get(url)
+        """Retrieve and cache metadata."""
+        page = self.session.get(url)
         return page.json()["items"]
 
     def retrieve_station_data(self, station_id):
         """
         Returns the last 24 hours of recordings for a given station.
         """
+        self.readings = {}
+        self.measure_metadata = {}
+
         station_url = self.page_url + station_id
         self.station_metadata = self.retrieve_metadata(station_url)
 
-        station_page = requests.get(station_url + "/readings?since=" + self.last_24_hrs)
-        station_data = station_page.json()["items"]
+        station_page = self.session.get(
+            f"{station_url}/readings?since={self.last_24_hrs}"
+        )
+        station_readings = station_page.json()["items"]
 
-        for datapoint in station_data:
-            measure = self.get_measure_name(datapoint)
-            if measure not in self.data.keys():
+        for reading in station_readings:
+            measure = self.get_measure_id(reading)
+            if measure not in self.readings.keys():
                 # initialise empty inner dict to hold timestamps,metadata and values
-                self.data[measure] = {}
-                self.data[measure]["timestamps"], self.data[measure]["values"] = [], []
+                self.readings[measure] = {}
+                (
+                    self.readings[measure]["timestamps"],
+                    self.readings[measure]["values"],
+                ) = [], []
                 self.measure_metadata[measure] = self.retrieve_metadata(
-                    datapoint["measure"]
+                    reading["measure"]
                 )
-            timestamp = datapoint["dateTime"]
-            value = datapoint["value"]
-            self.data[measure]["timestamps"].append(timestamp)
-            self.data[measure]["values"].append(value)
 
-    def get_rounded_timestamp(self):
-        """Returns the current timestamp rounded down to the nearest 15-minute increment."""
+            self.readings[measure]["timestamps"].append(reading["dateTime"])
+            self.readings[measure]["values"].append(reading["value"])
+
+    def get_rounded_current_time(self):
+        """
+        Returns the current timestamp rounded down to the nearest 15-minute increment.
+        """
         now = dt.datetime.now(dt.timezone.utc)
         rounded_minutes = now.minute - (
             now.minute % 15
@@ -57,48 +81,48 @@ class MeasurementTool:
         return rounded_time.strftime("%Y-%m-%dT%H:%M:%SZ")
 
     def subtract_24_hours(self, timestamp: str) -> str:
-        """Subtracts 24 hours from a given timestamp and returns it in the same format."""
+        """
+        Subtracts 24 hours from a given timestamp and returns it in the same format.
+        """
         datetime = dt.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%SZ")
         new_dt = datetime - dt.timedelta(hours=24)
         return new_dt.strftime("%Y-%m-%dT%H:%M:%SZ")
 
-    def get_measure_name(self, data):
-        return "-".join(
-            data["measure"].split("/")[-1].split("-")[1:]
-        )  # Extract measure name
-
-    def create_stations_dict(self):
-        stations_data = {}
-        for entry in self.entries:
-            label = entry["label"]
-            if isinstance(label, list):
-                label = entry["label"][0]
-            stations_data[entry["stationReference"]] = label.title()
-        return stations_data
-
     def plot_measure(self, df):
         """Plots the time series data."""
-        # Convert timestamp to datetime
+
         df = df.sort_values("timestamps")
         df["timestamps"] = pd.to_datetime(df["timestamps"])
-        # Plot
+
         fig, ax = plt.subplots(figsize=(10, 5))
         ax.plot(df["timestamps"], df["values"], marker="o", linestyle="-")
 
-        # Formatting
         ax.set_xlabel("Timestamp")
         ax.set_ylabel("Value")
         ax.set_title("Time Series Data")
-        plt.xticks(rotation=45)  # Rotate x-axis labels for readability
+        plt.xticks(rotation=45)  # readability
         ax.grid()
         plt.tight_layout()
-        st.pyplot(fig)  # Display the plot
+        st.pyplot(fig)
+
+    def get_measure_id(self, data):
+        """
+        Retrieves the unique identifier used for a measure.
+        eg. {root}/id/measures/1029TH-level-downstage-i-15_min -> level-downstage-i-15_min
+        """
+        return "-".join(data["measure"].split("/")[-1].split("-")[1:])
 
     def create_measure_label(self, measure_metadata):
-        return measure_metadata["parameterName"] + " - " + measure_metadata["qualifier"]
+        """
+        Creates a formatted measure label based on metadata.
+        """
+        return f"{measure_metadata['parameterName']} - {measure_metadata['qualifier']}"
 
     def create_station_label(self, station_id):
-        return station_id + ": " + tool.stations[station_id]
+        """
+        Creates a formatted station labebl based on station name and ID.
+        """
+        return f"{station_id}: {tool.stations[station_id]}"
 
 
 # Create an instance of MeasurementTool
@@ -124,8 +148,7 @@ if station_id:
     tool.retrieve_station_data(station_id)
 
     # Get the list of available measures (keys from `self.data`)
-    measures = list(tool.data.keys())
-    print(list(tool.data.keys()))
+    measures = list(tool.readings.keys())
 
     def get_measure_name(measure_id):
         return tool.create_measure_label(tool.measure_metadata[measure_id])
@@ -137,6 +160,6 @@ if station_id:
 
     # Fetch and plot data when the measure is selected
     if measure_name:
-        df = pd.DataFrame(tool.data[measure_name])
+        df = pd.DataFrame(tool.readings[measure_name])
         st.write(df)  # Display the data in a table
         tool.plot_measure(df)  # Plot the data
